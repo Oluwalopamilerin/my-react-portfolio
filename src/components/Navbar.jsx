be098b0eb4ca263fc0181df345600d6e7165e461
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-20 05:23:48 +0100

    created the components folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {FaBars, FaTimes} from "react-icons/fa"
+import logo from "../assets/logo1.png"
+
+const Navbar = () => {
+  return (
+
+      <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+          <img src={logo} alt="logo" style={{width:"50px"}}/>
+        </div>
+        {/* Menu */}
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        {/* Hamburger */}
+        <div></div>
+
+        {/* Mobile menu */}
+        <div></div>
+
+        {/* Social icons */}
+        <div></div>
+    </div>
+    
+   
+  )
+}
+
+export default Navbar
