02343c0ae82f3b52c42792a8fae2917af51da7ae
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-23 21:19:05 +0100

    completed the navbar design for mobile and desktop menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaBars, FaTimes} from "react-icons/fa"
 import logo from "../assets/logo1.png"
 
 const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false)
+
+  const handleHamburgerClick = () => {
+    setMobileMenu(!mobileMenu)
+  }
   return (
 
       <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
@@ -10,32 +15,32 @@ const Navbar = () => {
           <img src={logo} alt="logo" style={{width:"50px"}}/>
         </div>
         {/* Menu */}
-        <div>
-          <ul className='flex'>
+       
+          <ul className='hidden md:flex'>
             <li>Home</li>
             <li>About</li>
             <li>Skills</li>
             <li>Work</li>
             <li>Contact</li>
           </ul>
-        </div>
+        
 
         {/* Hamburger */}
-        <div>
-          <FaBars className='hidden'/>
+        <div onClick={handleHamburgerClick} className='md:hidden z-10'>
+          {!mobileMenu?<FaBars />:<FaTimes/>}
         </div>
 
         {/* Mobile menu */}
-        <ul className='hidden'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
-          </ul>
+        <ul className={!mobileMenu?"hidden": ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center' }>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Contact</li>
+        </ul>
 
         {/* Social icons */}
-        <div className='hidden'></div>
+        <div className='hidden'> </div>
     </div>
     
    
