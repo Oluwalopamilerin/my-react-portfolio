007be65f5f8c8fa384ffe4ad4790bc129efd5ca8
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-24 21:53:12 +0100

    WIP

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen bg-[#0a192f]'>
+    <div name="about" className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div>
+          <p>About</p>
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
 
     </div>
   )
