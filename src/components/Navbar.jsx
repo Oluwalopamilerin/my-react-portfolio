f246290fd089ef1fffdc0d865072ad34bc42be94
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-28 14:58:10 +0100

    added the react scroll link elememt to the navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Link } from 'react-scroll';
 import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from "react-icons/fa";
 import { HiOutlineMail } from 'react-icons/hi';
 import {BsFillPersonLinesFill} from "react-icons/bs";
@@ -7,7 +8,7 @@ import logo from "../assets/logo1.png"
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false)
 
-  const handleHamburgerClick = () => {
+  const handleClick = () => {
     setMobileMenu(!mobileMenu)
   }
   return (
@@ -19,26 +20,91 @@ const Navbar = () => {
         {/* Menu */}
        
           <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li> 
+              <Link 
+                to="home" 
+                smooth={true}  
+                duration={500} >
+                Home
+              </Link>
+            </li>
+            <li><Link 
+                to="about" 
+                smooth={true}  
+                duration={500} >
+                About
+              </Link></li>
+
+            <li><Link 
+                to="skills" 
+                smooth={true}  
+                duration={500} >
+                Skills
+              </Link></li>
+
+            <li><Link 
+                to="work" 
+                smooth={true}  
+                duration={500} >
+                Work
+              </Link></li>
+
+            <li><Link 
+                to="contact" 
+                smooth={true}  
+                duration={500} >
+                Contact
+              </Link></li>
           </ul>
         
 
         {/* Hamburger */}
-        <div onClick={handleHamburgerClick} className='md:hidden z-10'>
+        <div onClick={handleClick} className='md:hidden z-10'>
           {!mobileMenu?<FaBars />:<FaTimes/>}
         </div>
 
         {/* Mobile menu */}
         <ul className={!mobileMenu?"hidden": ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center' }>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Work</li>
-            <li className='py-6 text-4xl'>Contact</li>
+            <li className='py-6 text-4xl'>
+              <Link 
+              onClick={handleClick}
+                to="home" 
+                smooth={true}  
+                duration={500} >
+                Home
+              </Link></li>
+            <li className='py-6 text-4xl'>
+              <Link 
+               onClick={handleClick}
+                to="about" 
+                smooth={true}  
+                duration={500} >
+                About
+              </Link></li>
+            <li className='py-6 text-4xl'>
+              <Link 
+               onClick={handleClick}
+                to="skills" 
+                smooth={true}  
+                duration={500} >
+                Skills
+              </Link></li>
+            <li className='py-6 text-4xl'>
+              <Link 
+               onClick={handleClick}
+                to="work" 
+                smooth={true}  
+                duration={500} >
+                Work
+              </Link></li>
+            <li className='py-6 text-4xl'>
+              <Link 
+               onClick={handleClick}
+                to="contact" 
+                smooth={true}  
+                duration={500} >
+                Contact
+              </Link></li>
         </ul>
 
         {/* Social icons */}
