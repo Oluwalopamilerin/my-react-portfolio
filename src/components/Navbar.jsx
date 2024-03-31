625f488aa4395369c71b70f8748d29d8daca01d0
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-31 23:58:39 +0100

    adding the link to social media accounts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -130,7 +130,7 @@ const Navbar = () => {
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a className='flex justify-between items-center w-full text-gray-300' 
-            href='/'>Email <HiOutlineMail size={30}/></a>
+            href=''>Email <HiOutlineMail size={30}/></a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a className='flex justify-between items-center w-full text-gray-300' 
