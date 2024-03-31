2d3efb9fe25eaa4d12bd6f94f883b3f6aa1897f9
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-31 21:47:59 +0100

    added social media links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,11 +122,11 @@ const Navbar = () => {
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a className='flex justify-between items-center w-full text-gray-300' 
-            href='/'>LinkedIn <FaLinkedin size={30}/></a>
+            href='linkedin.com/in/deborah--oyewole' target='_blank'>LinkedIn <FaLinkedin size={30}/></a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a className='flex justify-between items-center w-full text-gray-300' 
-            href='/'>Github <FaGithub size={30}/></a>
+            href='https://github.com/Oluwalopamilerin' target='_blank'>Github <FaGithub size={30}/></a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a className='flex justify-between items-center w-full text-gray-300' 
