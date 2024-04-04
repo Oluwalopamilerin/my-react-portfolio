31121ecdce7144e904fd7a82e671f0dbd1b10354
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-04-04 11:20:05 +0100

    code refactoring

--- src/components/Work/WorkData.jsx
+++ src/components/Work/WorkData.jsx
@@ -2,7 +2,8 @@ import Project1 from "../../assets/projects/2561CFE8-D067-4720-BEB0-961C1D84F534
 import Project2 from "../../assets/projects/7081EAC3-B109-4171-830E-2B080338E725_1_201_a.jpeg";
 import Project3 from "../../assets/projects/315F7555-7CB4-42E6-B0C0-54D7F557C255_1_201_a.jpeg";
 import Project4 from "../../assets/projects/EFB995C5-C90D-4440-9848-2100A3545276_1_201_a.jpeg";
-import Project5 from "../../assets/projects/8EA96AA3-4368-454A-9787-D5AF19B73B9D_4_5005_c.jpeg";
+import Project5 from "../../assets/projects/8EA96AA3-4368-454A-9787-D5AF19B73B9D_1_201_a.jpeg";
+import Project6 from "../../assets/projects/workImg.jpeg";
 
 const projectCardData = [
   {
@@ -12,26 +13,26 @@ const projectCardData = [
   },
   {
     backgroundImage: Project2,
-    demo: "https://my-movie-app-with-react.netlify.app/",
-    code: "https://github.com/Oluwalopamilerin/Movie-app-with-react.git",
+    demo: "https://oluwalopamilerin.github.io/Todo-list-app/",
+    code: "https://github.com/Oluwalopamilerin/Todo-list-app.git",
   },
   {
     backgroundImage: Project3,
-    demo: "https://my-movie-app-with-react.netlify.app/",
-    code: "https://github.com/Oluwalopamilerin/Movie-app-with-react.git",
+    demo: "https://oluwalopamilerin.github.io/Login-registration-website/",
+    code: "https://github.com/Oluwalopamilerin/Login-registration-website.git",
   },
   {
     backgroundImage: Project4,
-    demo: "https://my-movie-app-with-react.netlify.app/",
-    code: "https://github.com/Oluwalopamilerin/Movie-app-with-react.git",
+    demo: "https://oluwalopamilerin.github.io/Calculator-app/",
+    code: "https://github.com/Oluwalopamilerin/Calculator-app.git",
   },
   {
     backgroundImage: Project5,
-    demo: "https://my-movie-app-with-react.netlify.app/",
-    code: "https://github.com/Oluwalopamilerin/Movie-app-with-react.git",
+    demo: "https://oluwalopamilerin.github.io/Weather-app/",
+    code: "https://github.com/Oluwalopamilerin/Weather-app.git",
   },
   {
-    backgroundImage: Project5,
+    backgroundImage: Project6,
     demo: "https://my-movie-app-with-react.netlify.app/",
     code: "https://github.com/Oluwalopamilerin/Movie-app-with-react.git",
   }
