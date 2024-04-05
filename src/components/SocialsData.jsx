77a7f57dcaa8c051d886e9f58692da5069784480
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-04-05 22:24:55 +0100

    code refactoring

---
+++ src/components/SocialsData.jsx
@@ -0,0 +1,28 @@
+import {FaGithub, FaLinkedin} from "react-icons/fa";
+import { HiOutlineMail } from 'react-icons/hi';
+import {BsFillPersonLinesFill} from "react-icons/bs";
+
+const data = [
+  {
+    logo: FaLinkedin,
+    href: "https://www.linkedin.com/in/deborah--oyewole/",
+    name: "LinkedIn"
+  },
+  {
+    logo: FaGithub,
+    href: "https://github.com/Oluwalopamilerin",
+    name: "GitHub"
+  },
+  {
+    logo: HiOutlineMail,
+    href: "mailto:pamilerinoyewole06@gmail.com",
+    name: "Email"
+  },
+  {
+    logo: BsFillPersonLinesFill,
+    href: "tel: +2348130107130",
+    name: "Hotline"
+  }
+]
+
+export default data
