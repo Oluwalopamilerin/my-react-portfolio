6f386cf43dc325a1b11b99652bffeddfdb51e8bc
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-04-06 11:19:20 +0100

    code refactoring

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Socials = ({Logo, href, name}) => {
+const Socials = ({Logo, href, name, color}) => {
   return (
     <div>
       <ul>
-        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+        <li className={`w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 text- ${color}`}>
           <a className='flex justify-between items-center w-full text-gray-300' 
           href= {href} target='_blank'>{name}  <Logo size={30}/></a>
         </li>
